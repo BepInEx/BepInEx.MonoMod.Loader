a2b7030e9b262e3ff1c14b036583482ca4ca601f
Bepis <36346617+bbepis@users.noreply.github.com>
2019-04-06 15:42:06 +1100

    Basic MonoMod patcher support

---
+++ BepInEx.MonoMod.Loader/MonoModLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using BepInEx.Logging;
+using Mono.Cecil;
+
+namespace BepInEx.MonoMod.Loader
+{
+	public static class Patcher
+	{
+		public static IEnumerable<string> TargetDLLs { get; } = new[] { "Assembly-CSharp.dll" };
+
+		private static ManualLogSource Logger = Logging.Logger.CreateLogSource("MonoMod");
+
+		public static void Patch(AssemblyDefinition assembly)
+		{
+			string monoModPath = Path.Combine(Paths.BepInExRootPath, "monomod");
+
+			if (!Directory.Exists(monoModPath))
+				Directory.CreateDirectory(monoModPath);
+
+			using (var monoModder = new RuntimeMonoModder(assembly, Logger))
+			{
+				monoModder.LogVerboseEnabled = false;
+				monoModder.PerformPatches(monoModPath);
+			}
+		}
+	}
+}
---
+++ BepInEx.MonoMod.Loader/RuntimeMonoModder.cs
@@ -0,0 +1,61 @@
+﻿using BepInEx.Logging;
+using Mono.Cecil;
+using MonoMod;
+
+namespace BepInEx.MonoMod.Loader
+{
+	public class RuntimeMonoModder : MonoModder
+	{
+		protected ManualLogSource Logger { get; set; }
+
+		public RuntimeMonoModder(AssemblyDefinition assembly, ManualLogSource logger)
+		{
+			Module = assembly.MainModule;
+			Logger = logger;
+		}
+
+		public override void Log(object value)
+			=> Logger.LogMessage(value);
+
+		public override void Log(string value)
+			=> Logger.LogMessage(value);
+
+		public override void LogVerbose(object value)
+		{
+			if (!LogVerboseEnabled)
+				return;
+
+			Logger.LogDebug(value);
+		}
+
+		public override void LogVerbose(string value)
+		{
+			if (!LogVerboseEnabled)
+				return;
+
+			Logger.LogDebug(value);
+		}
+
+		public void PerformPatches(string modDirectory)
+		{
+			Read();
+
+			Log("[Main] Scanning for mods in directory.");
+			ReadMod(modDirectory);
+
+			MapDependencies();
+
+			Log("[Main] mm.AutoPatch();");
+			AutoPatch();
+
+			Log("[Main] Done.");
+		}
+
+		public override void Dispose()
+		{
+			Module = null;
+
+			base.Dispose();
+		}
+	}
+}
