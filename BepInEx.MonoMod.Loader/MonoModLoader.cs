1b995bc753c095c4ed42503e39e3908165ca1637
Bepis <36346617+bbepis@users.noreply.github.com>
2019-04-09 08:59:28 +1000

    Fix missing dependency directories

--- BepInEx.MonoMod.Loader/MonoModLoader.cs
+++ BepInEx.MonoMod.Loader/MonoModLoader.cs
@@ -13,14 +13,33 @@ namespace BepInEx.MonoMod.Loader
 
 		public static void Patch(AssemblyDefinition assembly)
 		{
+			string assemblyFolder = new System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath;
+
 			string monoModPath = Path.Combine(Paths.BepInExRootPath, "monomod");
 
 			if (!Directory.Exists(monoModPath))
 				Directory.CreateDirectory(monoModPath);
 
+			string[] resolveDirectories =
+			{
+				Paths.BepInExAssemblyDirectory,
+				Paths.ManagedPath,
+				Paths.PatcherPluginPath,
+				assemblyFolder,
+				Paths.PluginPath
+			};
+
 			using (var monoModder = new RuntimeMonoModder(assembly, Logger))
 			{
 				monoModder.LogVerboseEnabled = false;
+
+				monoModder.DependencyDirs.AddRange(resolveDirectories);
+
+				var resolver = (BaseAssemblyResolver)monoModder.AssemblyResolver;
+
+				foreach (var dir in resolveDirectories)
+					resolver.AddSearchDirectory(dir);
+
 				monoModder.PerformPatches(monoModPath);
 			}
 		}
