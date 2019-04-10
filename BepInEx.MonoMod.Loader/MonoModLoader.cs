238ba23682c8fa8348380293bc87c504e5b6b165
Bepis <36346617+bbepis@users.noreply.github.com>
2019-04-10 17:34:50 +1000

    Use a custom assembly resolver instead of folder matching

--- BepInEx.MonoMod.Loader/MonoModLoader.cs
+++ BepInEx.MonoMod.Loader/MonoModLoader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using BepInEx.Logging;
 using Mono.Cecil;
 
@@ -12,39 +13,67 @@ namespace BepInEx.MonoMod.Loader
 
 		private static ManualLogSource Logger = Logging.Logger.CreateLogSource("MonoMod");
 
+		public static string[] ResolveDirectories { get; set; } =
+		{
+			Paths.BepInExAssemblyDirectory,
+			Paths.ManagedPath,
+			Paths.PatcherPluginPath,
+			Paths.PluginPath
+		};
+
 		public static void Patch(AssemblyDefinition assembly)
 		{
 			Environment.SetEnvironmentVariable("MONOMOD_DMD_TYPE", "Cecil");
-
-			string assemblyFolder = new System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath;
-
+			
 			string monoModPath = Path.Combine(Paths.BepInExRootPath, "monomod");
 
 			if (!Directory.Exists(monoModPath))
 				Directory.CreateDirectory(monoModPath);
-
-			string[] resolveDirectories =
-			{
-				Paths.BepInExAssemblyDirectory,
-				Paths.ManagedPath,
-				Paths.PatcherPluginPath,
-				assemblyFolder,
-				Paths.PluginPath
-			};
+			
 
 			using (var monoModder = new RuntimeMonoModder(assembly, Logger))
 			{
 				monoModder.LogVerboseEnabled = false;
 
-				monoModder.DependencyDirs.AddRange(resolveDirectories);
+				monoModder.DependencyDirs.AddRange(ResolveDirectories);
 
 				var resolver = (BaseAssemblyResolver)monoModder.AssemblyResolver;
 
-				foreach (var dir in resolveDirectories)
+				foreach (var dir in ResolveDirectories)
 					resolver.AddSearchDirectory(dir);
+
+				resolver.ResolveFailure += ResolverOnResolveFailure;
 
 				monoModder.PerformPatches(monoModPath);
 			}
+		}
+
+		private static AssemblyDefinition ResolverOnResolveFailure(object sender, AssemblyNameReference reference)
+		{
+			foreach (var directory in ResolveDirectories)
+			{
+				var potentialDirectories = new List<string> { directory };
+
+				potentialDirectories.AddRange(Directory.GetDirectories(directory, "*", SearchOption.AllDirectories));
+
+				var potentialFiles = potentialDirectories.Select(x => Path.Combine(x, $"{reference.Name}.dll"))
+					.Concat(potentialDirectories.Select(x => Path.Combine(x, $"{reference.Name}.exe")));
+
+				foreach (string path in potentialFiles)
+				{
+					if (!File.Exists(path))
+						continue;
+
+					var assembly = AssemblyDefinition.ReadAssembly(path, new ReaderParameters(ReadingMode.Deferred));
+
+					if (assembly.Name.Name == reference.Name)
+						return assembly;
+
+					assembly.Dispose();
+				}
+			}
+
+			return null;
 		}
 	}
 }
